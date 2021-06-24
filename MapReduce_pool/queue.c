c2cb36b20ecc92aec11843964c214334fa916b91
ilkclord <smpss96243@gmail.com>
2021-06-24 22:03:42 +0800

    MPMC ringbuffer queue

--- MapReduce_pool/queue.c
+++ MapReduce_pool/queue.c
@@ -26,9 +26,9 @@ int m = move ;
 int v[move] ;
 int rm = 0 , wm = 0 ;
 int i = 0 ;
-pthread_t thread[3] ;
+pthread_t thread[4] ;
 pthread_mutex_t  lock;
-
+int l = 1 ;
 struct tn{
   queue * q ;
   int id ;
@@ -36,7 +36,7 @@ struct tn{
 
 void w(id * id){
   queue *  q = id->q ;
-  for( ; i < test ;){
+  for( ; q->mode == act ;){
     info * tmp ;
     job * tmp0 ;
     int ji = atomic_fetch_add(&i , 1) ;
@@ -45,39 +45,30 @@ void w(id * id){
       printf("ops\n");
     if(!job_init(&tmp0 , tmp , &foo))
       printf("oops\n");
-    while(atomic_load(&m) > 0){
-       
-       if(!queue_w( q,tmp0,id->id , ji )){
-         //atomic_fetch_sub(&m ,1) ;
-         //printf("w_blocking\n") ;
-        //queue_state(q) ;
-         }
-       else{
-         //job_info(tmp0) ;
-         //atomic_fetch_sub(&m ,1) ;
-         atomic_fetch_add(&wm , 1);
-         break ;
-       }
-    }
+    while(!queue_w( q,tmp0,id->id , ji ) && (q->mode == act)) ;
     if(m <= 0 )
       break ;
   }
-
+  if(atomic_load(&q->mode)== act && atomic_fetch_sub(&l  , 1)){
+        queue_stop(q , 1) ;
+  }
+  printf("done\n") ;
 }
 void r(queue * q){
-    job * j ;
+    job * j  = NULL;
     while(atomic_load(&m) > 0){
-      if(!queue_r(q , &j)){
-           //printf("r_blocking %d\n" , q->entry) ;
-            //queue_state(q) ;a
-      }
-      else{
-        //job_info(j) ;
+       while(!queue_r(q , &j) && (q->mode == act)) ;
+         //printf("r") ;
        rm++ ;
        atomic_fetch_sub(&m ,1) ;
-      }
+       if(j != NULL){
+        job_destroy(j);
+        j = NULL ;
+       }
     }
-    load(q , 0) ;
+    if(atomic_load(&q->mode) == act && atomic_fetch_sub(&l  , 1))
+        queue_stop(q , 1) ;
+    printf("dones\n") ;
 }
 
 int main(){
@@ -93,12 +84,17 @@ int main(){
   pthread_create(&thread[0] , NULL ,(void *)r ,t );
   pthread_create(&thread[1] , NULL , (void *)w  , a) ;
   pthread_create(&thread[2] , NULL , (void *)w , b) ;
+  pthread_create(&thread[3] , NULL , (void *)r , t) ;
+  pthread_join(thread[3] , NULL) ;
   pthread_join(thread[0] , NULL) ;
   pthread_join(thread[1] , NULL) ;
   pthread_join(thread[2] , NULL) ;
   //printf("m : %d rm : %d . wm : %d \n" ,m , rm ,wm) ;
-  //queue_state(t) ;
-  //queue_info(t) ;
+  queue_state(t) ;
+  queue_info(t) ;
+  free(a) ;
+  free(b) ;
+  queue_destroy(t) ;
   //for(int j  =0 ; j <move ; j++)
     //printf("%d" , v[j]) ;
   return 1;
--- MapReduce_pool/queue.h
+++ MapReduce_pool/queue.h
@@ -27,10 +27,20 @@
  */
 
 #define rbuffer_s 16
+/*
+ * node state
+ */
 #define readable 1
 #define writing  0
 #define raw      2
 
+/*
+ * flag for operation
+ */
+#define rc  2
+#define wc 4
+#define act 1
+#define rest 0
 uint8_t mask = 16 ;
 uint8_t wmask = 15 ;
 struct input{
@@ -53,20 +63,25 @@ int foo(info * i){
  */
 struct job{
     info * data ;
+    
     func * func ;
 } typedef job;
 
-struct jobqueue{
+struct jobqueue{`
     job * buffer[rbuffer_s] ;
     uint8_t state[rbuffer_s] ;
+    uint8_t rworker ;
+    uint8_t wworker ;
     uint8_t read ;
     uint8_t write ;
-    uint8_t entry ;
-    uint8_t load ;
+    int  entry ;
+    uint8_t mode ;
+    struct jobqueue * next ;
 } typedef queue ;
 
 uint8_t queue_state(queue * q) ;
 uint8_t queue_info(queue * q) ;
+uint8_t job_destroy(job * j) ;
 uint8_t info_init(info ** i , int a ,int b ,char * pwd){
     *i = malloc(sizeof(info)) ;
     if(!i)
@@ -93,52 +108,111 @@ uint8_t queue_init(queue ** q){
     (*q)->read = 0 ;
     (*q)->write = 0 ;
     (*q)->entry = 0 ;
-    (*q)->load = 1 ;
-    for(int i = 0 ; i < rbuffer_s ; i++)
+    (*q)->mode = 1 ;
+    (*q)->rworker =  (*q)->wworker = 0 ;
+    for(int i = 0 ; i < rbuffer_s ; i++){
       (*q)->state[i] = raw ;
+      (*q)->buffer[i] = NULL ;
+    }
+    queue_init(&((*q)->next)) ;
     return 1 ;
 }
 
-uint8_t load(queue * q ,int a){
-  q->load = a ;
-  return 1 ;
-}
 /*
  * Writing into queue
  */
 uint8_t queue_w(queue * q , job * j , int id , int ji){
-    if(( q->entry & mask)){
+    if(( atomic_load(&q->entry) > 16) && !(q->mode & wc)){
       return 0 ;
      }
     atomic_fetch_add(&q->entry , 1) ;
+    atomic_fetch_add(&q->wworker ,1 ) ;
     int w  = atomic_fetch_add(&q->write , 1) & wmask;
     while(atomic_load(&q->state[w]) != raw){
       /*
        * in the test program the write funtion keeps writing data into the buffer
        */
-      if(!q->load ){
+      if(q->mode & wc){
+        if(q->state[w] == raw)
+          break ;
         atomic_fetch_sub(&q->entry , 1) ;
-        return 0 ; 
+        job_destroy(j) ;
+        atomic_fetch_sub(&q->wworker , 1) ;
+        return 1 ; 
       }
     };
     atomic_store(&q->state[w] , writing) ;
     q->buffer[w] = j ;
     atomic_store(&q->state[w] , readable) ;
+    atomic_fetch_sub(&q->wworker ,1 ) ;
     return 1 ;
 }
 /*
  * Reading out the queue
  */
 uint8_t queue_r(queue * q ,job ** j ){
-    if(q->entry ==  0)
+    if(atomic_load(&q->entry) <=  0 && !(q->mode & rc))
       return 0 ;
     atomic_fetch_sub(&q->entry , 1) ;
+    atomic_fetch_add(&q->rworker ,1 ) ;
     int r = atomic_fetch_add(&q->read , 1) & wmask;
-    while(q->state[r] != readable) ;
+    while(q->state[r] != readable){
+      if(q->mode & rc ){
+         if(q->state[r] == readable)
+           break ;
+         atomic_fetch_add(&q->entry , 1) ;
+         *j = NULL ;
+         atomic_fetch_sub(&q->rworker , 1);
+         return 1 ; 
+      }
+    }
+    
     *j = q->buffer[r] ;
+    q->buffer[r] = NULL ;
     atomic_store(&q->state[r] , raw) ;
+    atomic_fetch_sub(&q->rworker ,1 ) ;
     return 1 ;
 }
+
+uint8_t job_destroy(job  * j){
+    free(j->data) ;
+    free(j) ;
+    return 1 ;
+}
+uint8_t queue_stop(queue *q , uint8_t mode){
+  /*
+   * 1 : rw
+   * 0 : wr
+   */
+  if(mode == 1){
+    q->mode = rc ;
+    while(q->rworker != 0) ;
+    q->mode = wc ;
+    while(q->wworker != 0) ;
+    q->mode = rest ;
+    return 1 ;
+  }
+  else if(mode == 0 ){
+    q->mode = wc ;  
+    while(q->wworker != 0) ;
+    q->mode = rc ;
+    while(q->rworker != 0) ;
+    q->mode = rest ;
+    return 1 ;
+  }
+  return 0 ;
+}
+
+uint8_t queue_destroy(queue * q){
+   queue_destroy(q->next) ;
+   for(int i = 0 ; i < rbuffer_s ; i++){
+     if(q->buffer[i] != NULL) 
+        job_destroy(q->buffer[i]) ;
+   }
+   free(q) ;
+   return 1 ;
+}
+
 
 /*
  * debug
@@ -158,18 +232,24 @@ uint8_t job_info(job * j){
 
 uint8_t queue_info(queue  * q){
     int j = q->read & wmask ;
-  for(int i  = 0 ; i < q->entry ; i++){
+  for(int i  = 0 ; i < rbuffer_s ; i++){
     printf("%d :" , j) ;
-    job_info(q->buffer[j]) ;
-    printf("state :%d \n" , q->state[j]) ;
+    if(q->state[j] == readable){
+      printf("%d :" , j) ;
+      job_info(q->buffer[j]) ;
+      printf("state :%d \n" , q->state[j]) ;
+    }
+    else 
+      printf("%d \n" , q->state[j]) ;
     j++ ;
     if(j > 15)
       j = 0 ;
+    
   }
   return 1 ;
 }
 
 uint8_t queue_state(queue *q){
-  printf("read : %d , write : %d  , entry : %d \n"  ,atomic_load(&q->read) &wmask ,atomic_load(&q->write)& wmask,atomic_load(&q->entry)) ;
+  printf("read : %d , write : %d  , entry : %d  load : %d\n"  ,atomic_load(&q->read) &wmask ,atomic_load(&q->write)& wmask,q->entry , q->mode) ;
   return 1 ;
 }
